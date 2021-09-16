809c5d62b9ce35115d5e045e6af6cba114def04d
A.Minozawa <a.minozawa@cybermissions.co.jp>
2021-09-16 16:45:30 +0900

    サインアップ完了

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path 
     assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
-      post user_path, params: { user: { 
+      post signup_path, params: { user: { 
         name:  "",
         email: "user@invalid",
         password: "foo",
@@ -15,5 +15,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error explanation'
     assert_select 'div..field_with_errors'
     assert_select 'li', "Name can't be blank"
+    assert_select 'li', "Email is invalid"
+    assert_select 'li', "Password confirmation doesn't match"
+    assert_select 'li', "Password is too short(long)"
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post user_path, params: { user: { 
+        name:  "Example User",
+        email: "user@example.com",
+        password:  "password",
+        password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'user/show'
+    assert_not flash.empty?
   end
 end
