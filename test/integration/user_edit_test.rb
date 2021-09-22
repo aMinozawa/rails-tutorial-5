0537e1afd3ec23ed5ae20e12d0b0c38ff4531da5
A.Minozawa <a.minozawa@cybermissions.co.jp>
2021-09-22 18:16:41 +0900

    フレンドリーフォワーディング

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -17,10 +17,11 @@ class UserEditTest < ActionDispatch::IntegrationTest
     assert_template 'user/edit'
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    assert_equal edit_user_url(@user), session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
@@ -32,5 +33,9 @@ class UserEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
+    delete logout_path
+    log_in_as(@user)
+    assert_redirected_to @user
+
   end
 end
