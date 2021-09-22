976b328ebc4400cdff264c2ff899163bb09f410e
A.Minozawa <a.minozawa@cybermissions.co.jp>
2021-09-22 17:06:13 +0900

    更新時ログインを要求

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,6 @@
 class UserController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -21,12 +22,35 @@ class UserController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
   end
 
-  private
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 end
