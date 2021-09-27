29131bfe95a4eda243c208de17069e598c7e9942
A.Minozawa <a.minozawa@cybermissions.co.jp>
2021-09-27 17:17:36 +0900

    ユーザーの更新表示削除機能完了

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,7 @@
 class UserController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -44,6 +45,12 @@ class UserController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to user_url
+  end
+
 private
 
   def user_params
@@ -64,5 +71,10 @@ private
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless @user == current_user
+  end
+
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
